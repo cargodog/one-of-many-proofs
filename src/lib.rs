b0533bbe8023721c28c9764259e93a5ce6b43b2a
cargodog <cargodog@protonmail.com>
2020-08-11 13:58:22 -0400

    Add doc example for ring sigs

--- src/lib.rs
+++ src/lib.rs
@@ -78,6 +78,64 @@
 //!     .is_ok());
 //! ```
 //!
+//! # Ring Signatures
+//! One particularly useful application of membership proofs is ring signatures. This can easily be
+//! accomplished by committing to some message before computing or verifying a proof. Consider the
+//! below example, signing and verifying a message from an anonymous member of the set:
+//! ```
+//! # use rand::rngs::OsRng; // You should use a more secure RNG
+//! # use one_of_many_proofs::proofs::{ProofGens, OneOfManyProofs};
+//! # use curve25519_dalek::scalar::Scalar;
+//! # use curve25519_dalek::ristretto::RistrettoPoint;
+//! # use merlin::Transcript;
+//! #
+//! // Set up proof generators
+//! let gens = ProofGens::new(5).unwrap();
+//!
+//! // Create the prover's commitment to zero
+//! let l: usize = 3; // The signer's commitment will be third in the set
+//! let v = Scalar::random(&mut OsRng); // Commit to any random value
+//! let r = Scalar::random(&mut OsRng); // You should use a more secure RNG
+//! let C_l = gens.commit(&v, &r).unwrap();
+//!
+//! // Build a random set containing the prover's commitment at index `l`
+//! let mut set = (1..gens.max_set_size())
+//!     .map(|_| RistrettoPoint::random(&mut OsRng))
+//!     .collect::<Vec<RistrettoPoint>>();
+//! set.insert(l, C_l);
+//!
+//! // Create a new commitment to the same value as `C_l`
+//! let r_new = Scalar::random(&mut OsRng); // You should use a more secure RNG
+//! let C_new = gens.commit(&v, &r_new).unwrap();
+//!
+//! // Compute a `OneOfMany` membership proof for this commitment
+//! let mut t = Transcript::new(b"OneOfMany-Test");
+//!
+//! // Commit to a message to be signed
+//! t.append_message(b"msg", b"Hello, World!");
+//!
+//! // Sign the message anonymously
+//! let proof = set.iter().prove_with_offset(&gens, &mut t.clone(), l, &(r - r_new), Some(&C_new)).unwrap();
+//!
+//! // Compute a `OneOfMany` membership proof for this commitment
+//! let mut t = Transcript::new(b"OneOfMany-Test");
+//!
+//! // Verification will fail, because this transcript doesn't commit to the same message
+//! assert!(set
+//!     .iter()
+//!     .verify_with_offset(&gens, &mut t.clone(), &proof, Some(&C_new))
+//!     .is_err());
+//!
+//! // Commit to a message to be signed
+//! t.append_message(b"msg", b"Hello, World!");
+//!
+//!  // Verification will now succeed, because this transcript commits to the signed message
+//! assert!(set
+//!     .iter()
+//!     .verify_with_offset(&gens, &mut t.clone(), &proof, Some(&C_new))
+//!     .is_ok());
+//! ```
+//!
 //! # Perfomance
 //! The proof(s) provided by this crate depend heavily on the
 //! [curve25519-dalek](https://docs.rs/curve25519-dalek) for elliptic curve operations on the
